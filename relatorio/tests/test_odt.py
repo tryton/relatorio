ed7a0fdd3ac18643cb857fd28b2fb11bca64e231
Cédric Krier <ced@b2ck.com>
2021-07-29 12:30:00 +0200

    Simplify copyright header
    bug64

--- relatorio/tests/test_odt.py
+++ relatorio/tests/test_odt.py
@@ -1,26 +1,6 @@
 # -*- encoding: utf-8 -*-
-###############################################################################
-#
-# Copyright (c) 2014-2017 Cedric Krier.
-# Copyright (c) 2007, 2008 OpenHex SPRL. (http://openhex.com) All Rights
-# Reserved.
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 3 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-
-
+# This file is part of relatorio.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import os
 import unittest
 from io import StringIO, BytesIO
