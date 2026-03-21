aef7b3704c8c9014e1457eb6636514497c9bfd23
Cédric Krier <ced@b2ck.com>
2026-03-21 16:46:15 +0100

    Remove support for PDF template
    Closes #75

--- relatorio/templates/__init__.py
+++ relatorio/templates/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of relatorio.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-plugins = ['base', 'opendocument', 'pdf', 'chart']
+plugins = ['base', 'opendocument', 'chart']
 
 for name in plugins:
     __import__('relatorio.templates.%s' % name)
